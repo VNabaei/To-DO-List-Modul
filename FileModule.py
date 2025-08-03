fe3b257d4ecd10c86e241a17e306eece2034e5a1
VNabaei <v.nabaee@gmail.com>
2025-08-03 16:25:41 +0330

    fix and feature : add a function and edit in FM.py

--- FileModule.py
+++ FileModule.py
@@ -1,71 +1,124 @@
 import os
 from datetime import datetime
 import getpass
+import csv
+# import  Lists_Task  // با جدول کار می کنم نظرم عوض شد
 
 
-#creat the tdl folder if not exist:
+
+# Creating folders for TDL files 
 TDL_Folder = "TodoLists app"
 if not os.path.exists(TDL_Folder):
     os.makedirs(TDL_Folder)
 
-def Create_New_list(name = str):
+
+# TDL file creator : 
+def Create_New_list(name :str):
+    '''
+    This function creates a file in csv format so that it can be loaded into the database.
     
-    file_path = os.path.join(TDL_Folder,f"{name}.txt")
+    Parameters
+    ---------- 
+    name : str
+    The name given to the toDOLIST
+    
+    Returns
+    -------
+    the path of File created
+    
+    '''
+    
+    file_path = os.path.join(TDL_Folder,f"{name}.csv") #ساخت مسیر فایل داده
+    
+    
+    #برسی وجود داشتن از قبل یا نه
     if os.path.exists(file_path):
-        ans = input ("this list is exists now!\nDo you want replace it ?(y/n)")
-        if ans.upper() != 'y':
+        #اگر موجود بود
+        ans = input ("this list is exists now!\nDo you want replace it ?(y/n)") 
+        if ans.upper() != 'y': #اگر نخواهد فایل جایگذین شود عملیات متوقف می شود
             return
-        else :
+        else : #اگر بخواهد جایگذین شود مسیر فایل قبلی حذف و مسیر جدید ساخته می شود
             delete_List(file_path)
             Create_New_list(name)
-    while True:
-        ddline_input = input("inter the DDline (Year/month/day) e.g. : 2025/07/31  : ") # در اینجا یه برسی مقدار داده شده هم اجرا شود.
-        if ddline_input :
-            try:
-                ddline_date = datetime.strptime(ddline_input, "%Y/%m/%d").date()
-                if ddline_date < datetime.today().date():
-                    print("DDline can not be in pass")
-                    continue
-            except ValueError:
-                print("the format is wrong")
-                print("Please enter the date in the format Y/M/D (e.g.: 2025/07/31)")
-                continue
-            break #exit the loop if date is valid
-        else:
-            ddline_date = datetime.today().date()
-            print("DDline is set to today")
-            break #exit the loop if no date is provided
-            
+    else :#درصورتی که فایل وجود نداشت
+        # TDList_File_Class = Lists_Task.To_Do_List(name) Creat the TDLclass 
+        TDLfile = open(file_path,"w",encoding= 'utf-8')
+        TDLfile.close()
+      
+    # ---- If desired, the file will be completed.   
+    # ---- the filed of colums table :
+    field_Of_TDL = ["ID","Title","Descreaption","DeadLine","Status","Creat_at","Edited_by","Create_bY"] 
+      
     input("Do you want add tasks to this list (y/n)? : ")
     if input().upper() == 'Y':
-        Add_Task(file_path)
+        Add_Task(file_path,field_Of_TDL)
     else:
         print("No tasks added to the list.")
+        def NulTDL_creator(file_path,field_Of_TDL):
+        
+        
+
+    
+      
+    # ---- File creation operation completed.    
     print(f"List {name} created successfully at {file_path}")
     return file_path
         
-        
+# Tasks Creator      
 def Add_Task (FileTDS):
+    '''
+    This function, in the todolist file,creates a task and fills in the rows that are the tasks .
+    
+    Parameters
+    ---------- 
+    name : path
+    The path of todolist 
+    
+    Returns
+    -------
+    the path of File created
+    
+    '''
     
     tasks = []
     while True :
-        task = input("input the task, for break,input nothing!")
+        task_name =input("input the task, for break,input nothing!")
         if task == "":
             break
-        tasks.append(task)
-    with open(FileTDS,"w") as f :
-        f.write(f"Title : {DDLine}\n") # از نوع لیست کلاس بساز
-        f.write(f"DeadLine : {DDLine}\n")
-        f.write(f"DeadLine : {DDLine}\n")
-        f.write(f"DeadLine : {DDLine}\n")
-        f.write(f"DeadLine : {DDLine}\n")
+        else :
+            task_input = {
+                "ID" : ID_Generator(FileTDS) 
+                ,"Title": task_name
+                ,"Descreaption" : input("add info")
+                ,"DeadLine" : Deadline_Creator()
+                ,"Status" : "Status"
+                ,"Creat_at" :datetime.today().date()
+                ,"Edited_by" : Editor()
+                ,"Create_bY" : Get_User()             
+             }
         
-        f.write(f"DeadLine : {DDLine}\n")
-        f.write(f"ceator : {Get_User}\n")
-        f.write("Task :\n")
-        for t in tasks :
-            f.write(f" {t} \n")
+
+        tasks.append(task)
+        
+    # with open(FileTDS,"w") as f :
+    #     f.write(f"Title : {DDLine}\n") # از نوع لیست کلاس بساز
+    #     f.write(f"DeadLine : {DDLine}\n")
+    #     f.write(f"DeadLine : {DDLine}\n")
+    #     f.write(f"DeadLine : {DDLine}\n")
+    #     f.write(f"DeadLine : {DDLine}\n")
+        
+    #     f.write(f"DeadLine : {DDLine}\n")
+    #     f.write(f"ceator : {Get_User}\n")
+    #     f.write("Task :\n")
+    #     for t in tasks :
+    #         f.write(f" {t} \n")
     print (f"task(s) saved in {FileTDS} successfully")
+ 
+ 
+# if no task to input :
+def NulTDL_creator(file_path,field_Of_TDL):
+    pass 
+
     
 def Show_List(FileTDS):
     if not os.path.exists(FileTDS):
@@ -113,3 +166,31 @@ def Get_User():
         return user
     except Exception:
         return "Unknown"
+
+def Deadline_Creator():
+    while True:
+        ddline_input = input("inter the DDline (Year/month/day) e.g. : 2025/07/31  : ") # در اینجا یه برسی مقدار داده شده هم اجرا شود.
+        if ddline_input :
+            try:
+                ddline_date = datetime.strptime(ddline_input, "%Y/%m/%d").date()
+                if ddline_date < datetime.today().date():
+                    print("DDline can not be in pass")
+                    continue
+                else:
+                    return ddline_date
+                    
+            except ValueError:
+                print("the format is wrong")
+                print("Please enter the date in the format Y/M/D (e.g.: 2025/07/31)")
+                continue
+            break #exit the loop if date is valid
+        else:
+            ddline_date = datetime.today().date()
+            print("DDline is set to today")
+            return ddline_date
+            break #exit the loop if no date is provided
+      
+def ID_Generator(FileTDS) :
+    with open(FileTDS , 'r') as f :
+        f.r
+       
